daa2853baff9d6bec9efbc975714671b4a5260b9
Rajasah2000 <rajasah30030@gmail.com>
2026-09-16 11:45:20 +0530

    Added today's c code

---
+++ output/primenumber.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    for(int i=2;i<=n-1;i++){
+        if(n%i==0){
+            printf("the given number is composite\n");
+            break;
+        }
+    }
+    return 0;
+}
